3df7e8f7c17ce76bafbc45cdf8731f28ccfafc9e
Ignacio Ainol <iggnaxios@gmail.com>
2019-11-26 10:27:44 -0300

    remove all products from menu options

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ const templateMenu = [
             {
                 label:'Remove All Products',
                 click(){
-
+                    mainWindow.webContents.send('products:remove-all');
                 }
         
             },
